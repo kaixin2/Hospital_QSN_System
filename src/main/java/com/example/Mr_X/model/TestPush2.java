21320208c020119a16f09efbd890ac46f46deea7
kaixin2 <2159289430@qq.com>
2022-05-26 16:57:38 +0800

    this is the second time to push from branch Mr_X

---
+++ src/main/java/com/example/Mr_X/model/TestPush2.java
@@ -0,0 +1,14 @@
+package com.example.Mr_X.model;
+
+/**
+ * @author KaiXin on 2022-05-26
+ * @version 1.8
+ * @description
+ * @date 2022-05-26 16:55
+ */
+public class TestPush2 {
+
+    public static void main(String[] args) {
+        System.out.println("this is the second file to testing Push");
+    }
+}
