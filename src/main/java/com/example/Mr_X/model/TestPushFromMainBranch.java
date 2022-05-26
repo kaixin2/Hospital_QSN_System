fd75d96887712420ecfc1db33d5c65a9bcbc634c
kaixin2 <2159289430@qq.com>
2022-05-26 16:35:35 +0800

    this is the fist time to push from branch Mr_X

---
+++ src/main/java/com/example/Mr_X/model/TestPushFromMainBranch.java
@@ -0,0 +1,15 @@
+package com.example.Mr_X.model;
+
+/**
+ * this class to testing push from idea to github at main branch
+ *
+ * @author KaiXin on 2022-05-26
+ * @version 1.8
+ * @description
+ * @date 2022-05-26 16:31
+ */
+public class TestPushFromMainBranch {
+
+
+}
+
