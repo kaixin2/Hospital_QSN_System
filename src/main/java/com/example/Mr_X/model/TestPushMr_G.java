7e01e26228163ddb2afbe4760d73d26f4bcaac4a
kaixin2 <2159289430@qq.com>
2022-05-26 17:30:42 +0800

    change file from new branch Mr_W

--- src/main/java/com/example/Mr_X/model/TestPushMr_G.java
+++ src/main/java/com/example/Mr_X/model/TestPushMr_G.java
@@ -4,11 +4,10 @@ package com.example.Mr_X.model;
  * @author KaiXin on 2022-05-26
  * @version 1.8
  * @description
- * @date 2022-05-26 16:55
+ * @date 2022-05-26 16:38
  */
-public class TestPush2 {
-
+public class TestPushMr_G {
     public static void main(String[] args) {
-        System.out.println("this is the second file to testing Push");
+        System.out.println("this branch is Mr_x");
     }
 }
