68c52cf875f54edae954f4828f5796ac42999ca5
aili <2739618360@qq.com>
2022-05-27 23:30:19 +0800

    add patient

---
+++ src/main/java/Mr_W/model/patient.java
@@ -0,0 +1,58 @@
+package Mr_W.model;
+
+public class patient {
+    String id;
+    String  name;
+    String sex;
+    String telephone;
+    String address;
+    String idNo;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setIdNo(String idNo) {
+        this.idNo = idNo;
+    }
+
+    public String getIdNo() {
+        return idNo;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+}
